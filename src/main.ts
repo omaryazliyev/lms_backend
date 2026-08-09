da12a339d022b7a8deba8522b5dbedb53c85f175
Omar <omaryazliyev2004@gmail.com>
2026-08-09 13:59:55 +0500

    Fix CORS to allow all origins for production

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { config } from './common/config/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://localhost:3000'],
+    origin: true,
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
